188ae65ab10038441be2bad82ecfe4f2bc8bfd58
CelioM <celiosobrinho82@gmail.com>
2020-07-22 11:21:52 -0300

    criados atributos de instancia nome e idade

--- orient_obj/pessoa.py
+++ orient_obj/pessoa.py
@@ -1,6 +1,15 @@
 class Pessoa:
+    def __init__(self, nome = None, idade = 35):
+        self.idade = idade
+        self.nome = nome
     def cumprimentar(self):
-        return 'Ola'
+        return f'Ola{id(self)}'
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Luciano')
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = "Celio"
+    print(p.nome)
+    print(p.idade)
